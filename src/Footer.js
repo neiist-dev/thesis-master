e3e1961a8c3469120dba5f92479d9e2d783d1b1d
cata <46010838+cmcarreiro@users.noreply.github.com>
2020-10-26 11:23:47 +0000

    footer: changed footer-social units from rem to vw
    changed footer-social units from rem to vw so that it it can resize without media queries. made logos bigger.

--- src/Footer.js
+++ src/Footer.js
@@ -37,10 +37,10 @@ class Footer extends React.Component {
                 </Row>
 
                 <Row id="footer-social">
-                    <Col sm={{ span: 1, offset: 4}}><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/gce.neiist" className="fa" id="fa-facebook"><FontAwesomeIcon icon={faFacebookF} /></a></Col>
-                    <Col sm={1}><a target="_blank" rel="noopener noreferrer" href="FIXME" className="fa" id="fa-instagram"><FontAwesomeIcon icon={faInstagram} /></a></Col>
-                    <Col sm={1}><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/11163917" className="fa" id="fa-linkedin"><FontAwesomeIcon icon={faLinkedinIn} /></a></Col>
-                    <Col sm={1}><a target="_blank" rel="noopener noreferrer" href="FIXME" className="fa" id="fa-twitch"><FontAwesomeIcon icon={faTwitch} /></a></Col>
+                    <Col xs={{ span: 1, offset: 4}}><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/gce.neiist" className="fa" id="fa-facebook"><FontAwesomeIcon icon={faFacebookF} /></a></Col>
+                    <Col xs={1}><a target="_blank" rel="noopener noreferrer" href="FIXME" className="fa" id="fa-instagram"><FontAwesomeIcon icon={faInstagram} /></a></Col>
+                    <Col xs={1}><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/11163917" className="fa" id="fa-linkedin"><FontAwesomeIcon icon={faLinkedinIn} /></a></Col>
+                    <Col xs={1}><a target="_blank" rel="noopener noreferrer" href="FIXME" className="fa" id="fa-twitch"><FontAwesomeIcon icon={faTwitch} /></a></Col>
                 </Row>
 
                 <Row id="footer-copyright"><p>© 2020 GCE-NEIIST</p></Row>
