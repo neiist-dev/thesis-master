a9a2f43fbbb3c3bef422e85836fa6d7e3a76dcf5
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2020-11-02 23:43:54 +0000

    [LandingSection] content passed as prop

--- src/Components/LandingSection/LandingSection.js
+++ src/Components/LandingSection/LandingSection.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './LandingSection.css';
 
-const HomeSection = ({ color }) => {
-    return(
-        <section  id="home-section" className={color}>
-            <h1>Hello</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />Quidem inventore tempore, enim quo natus velit consequatur voluptatibus vero reiciendis voluptas magnam, eveniet animi. <br/> Exercitationem consequuntur maiores modi quae eveniet perferendis!</p>
-            <Button variant="info" size="sm">Click me!</Button>
+const HomeSection = ({ data: { type, title, body_text, button_text } }) => {
+    return (
+        <section id={type} className="home-section">
+            <h1>{title}</h1>
+            <p>{body_text}</p>
+            <Button variant="info" size="sm">{button_text}</Button>
         </section>
-    )    
+    )
 }
 
 export default HomeSection;
