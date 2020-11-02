db7918c907104c999060c05fb25a790aa3b13a1f
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2020-11-02 23:45:12 +0000

    [App] app now has state also is no longer function

--- src/App.js
+++ src/App.js
@@ -3,24 +3,44 @@ import React from 'react';
 //import './App.css';
 import Footer from './Components/Footer/Footer';
 import LandingSection from './Components/LandingSection/LandingSection';
+import GCESection from './Components/GCESection/GCESection';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  const landingSection = [];
-  for (let i = 0; i < 5; i++) {
-    if ( i % 2 === 0){
-      landingSection.push(<LandingSection color="blue"/>)
-    } else {
-      landingSection.push(<LandingSection color="red"/>)
+class App extends React.Component {
+  state = {
+    data: {
+      thesis: {
+        type: "thesis",
+        title: "GCE Thesis",
+        body_text: "Prestes a escolher a tua tese de mestrado?\nO GCE criou um serviço que te permite ver quais as propostas de tese de mestrado, classificadas por área de especialização!",
+        button_text: "Visitar GCE Thesis"
+      },
+      hashcode: {
+        type: "hashcode",
+        title: "Google Hash Code",
+        body_text: "A Google e o GCE trazem-te um desafio de programação!\nO Google Hash Code é um hackathon à escala global que te desafia a resolver um problema real em equipa!",
+        button_text: "Sobre Hash Code"
+      },
+      articles: {
+        type: "articles",
+        title: "Artigos & Entrevistas",
+        body_text: "Lê aqui todas as publicações dos nossos colaboradores e descobre o que te espera no mundo profissional.\nDe alunos para alunos!",
+        button_text: "Ver Todos os Artigos"
+      }
     }
   }
+  render() {
+    return (
+      <div>
+        <GCESection />
+        <LandingSection data={this.state.data.thesis} />
+        <LandingSection data={this.state.data.hashcode} />
+        <LandingSection data={this.state.data.articles} />
+        <Footer />
+      </div >
+    );
+  }
 
-  return (
-    <div>
-      {landingSection}
-      <Footer />
-    </div>
-  );
 }
 
 export default App;
