967310e1d2926e4c1e247a78105afdfac7090285
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2020-11-19 22:01:16 +0000

    [App] App uses NavigationBar, LandingPage and Footer components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-import Footer from './Components/Footer/Footer';
-import LandingSection from './Components/LandingSection/LandingSection';
-import GCESection from './Components/GCESection/GCESection';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
+import LandingPage from './Components/LandingPage/LandingPage';
+import Footer from './Components/Footer/Footer';
+import './App.css';
 
-class App extends React.Component {
-  state = {
-    data: {
-      thesis: {
-        type: "thesis",
-        title: "GCE Thesis",
-        body_text: "Prestes a escolher a tua tese de mestrado?\nO GCE criou um serviço que te permite ver quais as propostas de tese de mestrado, classificadas por área de especialização!",
-        button_text: "Visitar GCE Thesis"
-      },
-      hashcode: {
-        type: "hashcode",
-        title: "Google Hash Code",
-        body_text: "A Google e o GCE trazem-te um desafio de programação!\nO Google Hash Code é um hackathon à escala global que te desafia a resolver um problema real em equipa!",
-        button_text: "Sobre Hash Code"
-      },
-      articles: {
-        type: "articles",
-        title: "Artigos & Entrevistas",
-        body_text: "Lê aqui todas as publicações dos nossos colaboradores e descobre o que te espera no mundo profissional.\nDe alunos para alunos!",
-        button_text: "Ver Todos os Artigos"
-      }
-    }
-  }
-  render() {
-    return (
-      <div>
-        <GCESection />
-        <LandingSection data={this.state.data.thesis} />
-        <LandingSection data={this.state.data.hashcode} />
-        <LandingSection data={this.state.data.articles} />
-        <Footer />
-      </div >
-    );
-  }
-
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <LandingPage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
