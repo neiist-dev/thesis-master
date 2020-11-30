56be73bce90f9a62f19b28fb8c4b791163f2fd52
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2020-11-30 17:05:37 +0000

    [GceThesis] A lot of work ahead

--- src/Components/GceThesis/GceThesis.js
+++ src/Components/GceThesis/GceThesis.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './GceThesis.css';
 
-function GceThesis() {
-  return (
-    <p>GCE Thesis</p>
-  );
+class GceThesis extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            theses: null /*getThesis()   TODO*/,
+            thesesFiltered: null /*getThesis()   TODO*/
+        };
+    }
+    render() {
+        return (
+            <>
+                {/*<AreasButtons theses={this.state.theses}></AreasButtons>
+                <SearchBar theses={this.state.theses}></SearchBar>
+                <ButtonInstructions></ButtonInstructions>
+                <ThesisCards thesis={this.state.thesesFiltered}></ThesisCards>
+                */}
+                <h1>{/*this.state.thesesFiltered.length*/}BUÉS propostas de tese</h1>
+            </>
+        );
+    }
 }
 
+
+function ButtonInstructions() {
+    return (
+        <button>Instruções</button>
+    );
+}
 export default GceThesis;
+
+
