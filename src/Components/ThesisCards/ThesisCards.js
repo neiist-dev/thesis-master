a6a2dcae3fb604f559450a588ce37ad852e09c23
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2020-11-30 23:33:22 +0000

    [ThesisCards] Placeholder for layout

---
+++ src/Components/ThesisCards/ThesisCards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ThesisCards.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function ThesisCards() {
+    return (
+        <Col md={8} id="thesis-cards-container">
+        </Col>
+    );
+}
+export default ThesisCards;
+
+
