d4c287581382823367e64c3f6418f93bd81f44a8
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2020-12-07 17:10:10 +0000

    [App] Removed LandingPage and ArticlesInterviews

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
-import LandingPage from './Components/LandingPage/LandingPage';
 import Footer from './Components/Footer/Footer';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import ArticlesInterviews from './Components/ArticlesInterviews/ArticlesInterviews';
 import GceThesis from './Components/GceThesis/GceThesis';
 
 function App() {
@@ -14,8 +12,9 @@ function App() {
     <Router>
       <Container fluid>
         <NavigationBar />
+        {/*
         <Route exact path="/" component={LandingPage} />
-        <Route path="/artigos-entrevistas" component={ArticlesInterviews} />
+        */}
         <Route path="/gce-thesis" component={GceThesis} />
         <Footer />
       </Container>
