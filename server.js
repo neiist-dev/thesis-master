a42a341af34ffb2d51b251df047481e45cb1ee6f
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2021-01-15 18:42:43 +0000

    [server.js] Login is kinda done; Client-side cookie logic is yet to be implemented;
    Co-Authored-By: Catarina Carreiro <46010838+cmcarreiro@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const axios = require('axios')
+const path = require('path');
+const { response } = require('express');
 require('dotenv').config()
 
 const app = express();
 
-//servir o /client em www.xxx.
+/* https://github.com/hemakshis/Basic-MERN-Stack-App/blob/master/app.js */
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
-const FENIX_CLIENT_ID = process.env.FENIX_CLIENT_ID
-const FENIX_CLIENT_SECRET = process.env.FENIX_CLIENT_SECRET
-const FENIX_REDIRECT_URI = process.env.FENIX_REDIRECT_URI
+const {
+    FENIX_CLIENT_ID,
+    FENIX_CLIENT_SECRET,
+    FENIX_REDIRECT_URI
+} = process.env
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
     res.redirect(
         'https://fenix.tecnico.ulisboa.pt/oauth/userdialog' +
         `?client_id=${FENIX_CLIENT_ID}` +
@@ -19,43 +24,72 @@ app.get('/', (req, res) => {
 });
 
 app.get('/authorize', async (req, res, next) => {
-    const code = req.query.code;
 
+    const code = req.query.code;
     console.log("code:", code);
 
     try {
-        // get token with received code
         var response = await axios.post(
             'https://fenix.tecnico.ulisboa.pt/oauth/access_token' +
             `?client_id=${FENIX_CLIENT_ID}` +
             `&client_secret=${encodeURIComponent(FENIX_CLIENT_SECRET)}` +
             `&redirect_uri=${FENIX_REDIRECT_URI}` +
-            `&code=${code}` +
+            `&code=${encodeURIComponent(code)}` +
             '&grant_type=authorization_code'
         );
-
-        console.log("response:", response);
-
         if (response === undefined || response.status != 200) {
-            res.redirect("/oopsie");
+            console.log(error);
+            res.redirect("/error");
             return;
         }
     } catch (error) {
-        console.log("error:", error);
-        res.redirect("/oopsie");
+        console.log(error);
+        res.redirect("/error");
         return;
     }
 
-
-    const token = response.data.access_token;
-
-    console.log("token:", token);
+    const accessToken = response.data.access_token;
+    console.log("accessToken:", accessToken);
 
     try {
-        var userInfo = await RegisterFenixServices.get_user_info(token);
+        var response = await axios.get(
+            'https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person' +
+            `?access_token=${accessToken}`
+        );
+        if (response === undefined || response.status != 200) {
+            console.log(error);
+            res.redirect("/error");
+            return;
+        }
     } catch (error) {
-        console.log("error:", error);
-        res.redirect("/oopsie");
+        console.log(error);
+        res.redirect("/error");
+        return;
+    }
+
+    const personInformation = response;
+    console.log("personInformation:", personInformation.data.roles);
+
+    let isCurrentLMeicStudent = false;
+
+    personInformation.data.roles.forEach(role => {
+        console.log("role.registrations:", role.registrations);
+        if (role.registrations) {
+            role.registrations.forEach(registration => {
+                if (registration.acronym && ['LEIC-A', 'LEIC-T', 'MEIC'].includes(registration.acronym))
+                    isCurrentLMeicStudent = true;
+            })
+        }
+    });
+
+    console.log("isCurrentLMeicStudent:", isCurrentLMeicStudent);
+
+    if (isCurrentLMeicStudent) {
+        res.redirect("/done");
+        return;
+    }
+    else {
+        res.redirect("/failed");
         return;
     }
 })
